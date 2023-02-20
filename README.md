ad647f9d9d26477aef9061f7569ec9c4b5c24777
Dayana Alejandro <dayana200120@gmail.com>
2023-02-19 20:39:10 -0500

    adding navbar to react app and adding to readme install requirements

---
+++ README.md
@@ -0,0 +1,9 @@
+# monitoring-crypto
+
+Pulling from flask rest API server on [Backend Repo](https://github.com/Dayana20/monitoring-crypto)
+
+To build, run backend server and then type 'npm start' while inside the frontend directory.
+
+For bootstrap to work:
+Needed to install:
+* npm install --save bootstrap
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import UserList from './userList';
+import NavBar from './NavBar';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <NavBar />
     <App />
     <h4>User List:</h4>
 
