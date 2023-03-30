368839bcf326223f72ed6b7a5eec40b123ee4bc8
Dayana Alejandro <dayana200120@gmail.com>
2023-03-30 12:57:43 -0400

    adding advanced search bar that uses HATEOAS endpoint for dropdown

--- frontend/src/searchPage.js
+++ frontend/src/searchPage.js
@@ -1,22 +1,51 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import NavBar from "./NavBar"
-import UserList from "./userList"
-import Search from "./pages/Search/Search"
-import CoinList from "./coinList"
+import Form from "react-bootstrap/Form"
+import Button from "react-bootstrap/Button"
 import "./css/home.css"
-import Login from "./login" 
+import Select from "react-select"
+
 
 // search page that comes up after searching for coin/user/post
 // would show user + img or coin+logo or post snippet
 
 function SearchPage() {
     const [word, setWord] = useState("")
+    const [menuData, setMenuData] = useState([{}])
+
     console.log("word:",word)
+
+    useEffect(() => {
+      fetch("/options").then(
+        res => res.json()
+      ).then(
+          data => {
+            setMenuData(data)
+        }            
+      ). catch((error) => {
+        console.error("Error: ", error)
+      })
+    }, [])
+    let options = [{value:"All", label:"All"}]
+    if(typeof menuData == "undefined"){    
+      for(let i=0; i< menuData["Choices"].length;i++){
+        options.push({values: menuData["Choices"][i]["value"], label: menuData["Choices"][i]["label"]})
+      }
+    }
     return (
-      <div>
+      <>
         <NavBar/>
-        <div>
-          <Search word={word} setWord={setWord} />
+        <div id="searchbar" style={{marginTop:"5vh",marginBottom:"5vh"}}>
+            <Form className="d-flex justify-content-center">
+              <Select options={options}/>
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
         </div>
 
         <div>
@@ -30,7 +59,8 @@ function SearchPage() {
             
           </div>
         </div>
-      </div>
+        
+      </>
     )
   }
   
