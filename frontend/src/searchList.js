b333d6e35ccc2bbeca42cc80ec6d45a0fdd489fb
Dayana Alejandro <dayana200120@gmail.com>
2023-04-07 01:55:17 -0400

    updating search bar for filtering and updated HATEOAS feature to display specific filters

---
+++ frontend/src/searchList.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react"
+
+// https://dev.to/salehmubashar/search-bar-in-react-js-545l
+
+function UserNameList(searchBarInput){
+    const [data, setData] = useState([])
+	useEffect(() => {
+		fetch("/users/list/names").then(
+			res => res.json()
+		).then(
+			data => {
+				setData(data)
+			}            
+		). catch((error) => {
+			console.error("Error: ", error)
+		})
+	}, [])
+    const filteredUserData = data.filter((names) => {
+        // return all names if nothing specific is searched
+        if (searchBarInput.input === "") {
+            return names
+        }
+        else {
+            return names.includes(searchBarInput.input)   
+        }
+    })
+    return(
+        <>
+            {filteredUserData.map((item) => (
+                <div className="d-flex align-items-center" style={{border: "5px solid white", height:"8vw", marginLeft:"3vh"}}>
+                    <a href={"/Profile/"+item}>{item}</a>
+                </div>
+            ))}
+        </>
+    )
+}
+
+
+export default UserNameList
