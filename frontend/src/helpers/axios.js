55a44595cb02a8406004e5ab5da4502fddd1c642
Darren Chan <darrenchan9902@gmail.com>
2023-04-21 14:58:36 -0400

    Add api_local and api_prod.sh, Update API environment base URLs

--- frontend/src/helpers/axios.js
+++ frontend/src/helpers/axios.js
@@ -1,10 +1,6 @@
 import axios from "axios"
 
-// uses cloud mongodb
-const baseURL = process.env.REACT_APP_PROD_URL
-
-// uses local
-// const baseURL = process.env.REACT_APP_DEV_URL
+const baseURL = process.env.REACT_APP_BACKEND_BASE_URL
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
